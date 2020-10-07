000fd1d604bd45a79ba4bd00df348ae1f9dae899
Daiyaan Ijaz <3193289+cheesea3@users.noreply.github.com>
2020-10-06 21:01:31 -0400

    pass values from form and connect db

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php
+//connect to db
+$pdo = new PDO("mysql:localhost","root","");
+?>
+
+<?php if ($_SERVER["REQUEST_METHOD"] == "POST"): ?>
+What's up <?php echo htmlspecialchars($_POST["username"]);?>
+
+<?php else: ?>
+<form id="login" method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+    <label for="username">Username</label>
+    <input type="name" name="username" id="username" required/>
+    <br>
+    <label for="password" name="password">Password</label>
+    <input type="password" id="password" required/>
+    <input type="submit" value="submit"/>
+</form>
+<?php endif; ?>
